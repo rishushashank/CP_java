d7bddc0cff7956ade1c98002ce3ba407be64fe99
Rishu Kumar <123060785+rishushashank@users.noreply.github.com>
2023-02-18 10:20:13 +0530

    Add files via upload

---
+++ Recursion/subsequence_sumK.java
@@ -0,0 +1,16 @@
+package Recursion;
+//factorial using recursion
+import java.util.*;
+public class subsequence_sumK {
+    static int fact(int n){
+        if(n==0){
+            return 1;
+        }
+        return n*fact(n-1);
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(fact(n));
+    }
+}
